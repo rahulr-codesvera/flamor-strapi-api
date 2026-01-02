dcd74dfe9d0982a1449e3d9cc012031376e8db9c
rahulr-codesvera <rahulrajmk@codesvera.com>
2026-01-02 17:38:27 +0530

    Remove unnecessary path module usage in database configuration

--- config/database.js
+++ config/database.js
@@ -1,13 +1,8 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: env(
-        'DATABASE_FILENAME',
-        path.join(__dirname, '..', '..', 'data', 'data.db')
-      ),
+      filename: env('DATABASE_FILENAME'),
     },
     useNullAsDefault: true,
   },
